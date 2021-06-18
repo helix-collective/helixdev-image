bc8a86d2ae04ce9c7dba4e39c268a9861e9b5849
Paul Thompson <paul@helixta.com.au>
2021-06-19 01:16:28 +1000

    Install node, yarn, bazelisk & sdkman in tools

--- docker/01-install-base.sh
+++ docker/01-install-base.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+# Install base packages
+
 export DEBIAN_FRONTEND=noninteractive
 
 # Update first prior to installing packages from the standard repos
@@ -8,12 +10,6 @@ apt-get update
 
 # Add curl and apt-transport-https for installing from https repositories
 apt-get install -y curl apt-transport-https gnupg2
-
-# Install yarn, node, etc
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get -y install nodejs yarn
 
 # Install packages
 apt-get install -y \
@@ -54,19 +50,8 @@ dpkg -i docker-ce_19.03.8~3-0~ubuntu-bionic_amd64.deb
 rm /tmp/*.deb
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-# Install bazel from a binary release
-VERSION=2.1.1
-INSTALLER=bazel-$VERSION-installer-linux-x86_64.sh
-wget -q https://github.com/bazelbuild/bazel/releases/download/$VERSION/$INSTALLER
-chmod +x $INSTALLER
-./$INSTALLER
-rm $INSTALLER
-/usr/local/bin/bazel version
-rm -rf /root/.cache/bazel/
-
 # symlink nginx so that NginxRunnerTest can work correctly
 ln -s /usr/sbin/nginx /usr/local/bin/nginx 
 # Update permissions of default log and lib directories so that buildkite agent can run nginx without issues
 chown root:www-data /var/lib/nginx /var/log/nginx
 chmod 775 /var/lib/nginx /var/log/nginx
-
--- docker/02-install-tools.sh
+++ docker/02-install-tools.sh
@@ -3,3 +3,28 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Install yarn, node, etc
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get -y install nodejs yarn
+
+# Install bazelisk
+wget -q https://github.com/bazelbuild/bazelisk/releases/download/v1.9.0/bazelisk-linux-amd64
+chmod +x bazelisk-linux-amd64
+mv bazelisk-linux-amd64 /usr/local/bin/bazelisk
+ln -s /usr/local/bin/bazelisk /usr/local/bin/bazel
+
+# Download a version to cache in the docker image:
+bazel version
+
+# Install sdkman - See https://sdkman.io/install
+# Installing without Modifying Shell Config
+# Users will need to source ~/.sdkman/bin/sdkman-init.sh to get it
+curl -s "https://get.sdkman.io?rcupdate=false" | bash
+
+# Download a java version to cache in the docker image:
+set +x
+source ~/.sdkman/bin/sdkman-init.sh
+sdk install java 11.0.11.hs-adpt
+set -x
