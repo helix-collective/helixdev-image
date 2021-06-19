a6a2686e64c0080d5b4615dd5cde44373e7b8fd4
Paul Thompson <paul@helixta.com.au>
2021-06-20 09:24:21 +1000

    Remove install of yarn, node etc

--- docker/02-install-tools.sh
+++ docker/02-install-tools.sh
@@ -15,12 +15,6 @@ dvm install 1.10.3
 # Install dnit:
 deno install --allow-read --allow-write --allow-run --unstable -f --name dnit https://deno.land/x/dnit@dnit-v1.12.3/main.ts
 
-# Install yarn, node, etc
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get -y install nodejs yarn
-
 # Install bazelisk
 wget -q https://github.com/bazelbuild/bazelisk/releases/download/v1.9.0/bazelisk-linux-amd64
 chmod +x bazelisk-linux-amd64
