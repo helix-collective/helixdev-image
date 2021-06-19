08b74305aae02aed581cfd86690059e33096188d
Paul Thompson <paul@helixta.com.au>
2021-06-19 11:56:54 +1000

    Dev: bump devtest number

--- docker/dodo.py
+++ docker/dodo.py
@@ -34,7 +34,7 @@ def task_docker_build_helixdev_image():
     }
 
 def task_docker_push_helixdev_image():
-    tag = str(date.today()) + "-devtest"
+    tag = str(date.today()) + "-devtest-2"
     return {
         'doc' : 'push the helix dev docker image containing development tools',
         'actions': [
