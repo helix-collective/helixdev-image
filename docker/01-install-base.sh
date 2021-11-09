dd22c699d932ae8116b969b6da2ab9b75267a71e
Tim Docker <tim@dockerz.net>
2021-11-09 17:40:21 +1100

    Update to node 16 and nvm 8

--- docker/01-install-base.sh
+++ docker/01-install-base.sh
@@ -40,7 +40,7 @@ apt-get install -y \
 pip3 install doit pystache
 
 # Install yarn, node, etc
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get -y install nodejs yarn
